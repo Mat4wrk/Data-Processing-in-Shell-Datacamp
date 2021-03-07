46fcd48105f37d762f64771ffd5d004ca0b4338f
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-07 21:58:36 +0700

    Create Installing Python dependencies.sh

---
+++ 4.Data Pipeline on the Command Line/Installing Python dependencies.sh
@@ -0,0 +1,31 @@
+# Instantiate the requirements.txt document and add the scikit-learn library to the requirements.txt file.
+# Add scikit-learn to the requirements.txt file
+pip install "scikit-learn" > requirements.txt
+
+# Preview file content
+cat requirements.txt
+
+
+# Use pip to install the dependencies in the requirements.txt file.
+# Add scikit-learn to the requirements.txt file
+echo "scikit-learn" > requirements.txt
+
+# Preview file content
+cat requirements.txt
+
+# Install the required dependencies
+pip install -r requirements.txt
+
+
+# Use pip to list what Python libraries are already installed in your environment.
+# Add scikit-learn to the requirements.txt file
+echo "scikit-learn" > requirements.txt
+
+# Preview file content
+cat requirements.txt
+
+# Install the required dependencies
+pip install -r requirements.txt
+
+# Verify that Scikit-Learn is now installed
+pip list
