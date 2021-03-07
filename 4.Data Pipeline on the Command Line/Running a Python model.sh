915b882b4f14940cc5459f7b7789073937fa43ac
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-07 21:59:55 +0700

    Create Running a Python model.sh

---
+++ 4.Data Pipeline on the Command Line/Running a Python model.sh
@@ -0,0 +1,26 @@
+# Use pip to list and verify that the libraries specified in create_model.py have been installed.
+# Re-install requirements
+pip install -r requirements.txt
+
+# Preview Python model script for import dependencies
+cat create_model.py
+
+# Verify that dependencies are installed
+pip list
+
+
+# Run the Python script create_model.py from the command line.
+# Re-install requirements
+pip install -r requirements.txt
+
+# Preview Python model script for import dependencies
+cat create_model.py
+
+# Verify that dependencies are installed
+pip list
+
+# Execute Python model script, which outputs a pkl file
+python create_model.py
+
+# Verify that the model.pkl file has been created 
+ls
